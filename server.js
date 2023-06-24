69e4d44410800529bd5e239684cf312d919f6064
srijan <srijan.dhl21@gmail.com>
2023-06-24 11:50:15 +1000

    updated entire server.js

--- server.js
+++ server.js
@@ -1,32 +1,39 @@
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
-
+const app = express();
 import cors from "cors";
+const PORT = process.env.PORT || 8000;
 import path from "path";
 
 const __dirname = path.resolve();
-console.log(__dirname);
-const app = express();
-import taskRouter from "./src/router/taskRouter.js";
-const PORT = process.env.PORT || 8000;
+
+// connect mongodb
 import { mongoConnect } from "./src/config/mongoDB.js";
 
+// middlewares
 app.use(express.json());
 app.use(cors());
-app.use("/api/v1/task", taskRouter);
 app.use(express.static(__dirname + "/build"));
+
+// API endpoints
+import taskRouter from "./src/router/taskRouter.js";
+
+app.use("/api/v1/task", taskRouter);
+
 app.use("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
+
 mongoConnect()
   .then(() => {
-    app.listen(PORT, (error) => {
-      error
-        ? console.log(error.message)
+    app.listen(PORT, (err) => {
+      err
+        ? console.log(err.message)
         : console.log(`Server running at http://localhost:${PORT}`);
     });
   })
   .catch((error) => {
     console.log(error.message);
   });
+// open port for http request to access the server
